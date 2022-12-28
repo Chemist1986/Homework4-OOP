dad3f31719df7e04a4f72a43f124eb1d77ef0fbc
Vyacheslav <vmk_86@mail.ru>
2022-12-28 13:15:28 +0300

    save11

---
+++ Staff.java
@@ -0,0 +1,12 @@
+public class Staff implements Weapon {  //посох
+
+    @Override
+    public Integer damage() {
+        return 10;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Staff - %d", damage());
+    }
+}
